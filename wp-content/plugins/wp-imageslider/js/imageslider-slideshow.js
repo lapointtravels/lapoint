266aba61e2466e4deca15ad94f9d50278f58f90d
rickard <reekrd@gmail.com>
2018-05-03 09:31:23 +0200

    Trying to prevent up/down when swiping

--- wp-content/plugins/wp-imageslider/js/imageslider-slideshow.js
+++ wp-content/plugins/wp-imageslider/js/imageslider-slideshow.js
@@ -1,7 +1,7 @@
 ;(function ($, window, document, undefined) {
 
 	$.detectSwipe.threshold = 30;
-	$.detectSwipe.preventDefault = false; // or up and down is active and site doesnt scroll well
+	//$.detectSwipe.preventDefault = false; // or up and down is active and site doesnt scroll well
 
 	if (!Date.now) {
 	    Date.now = function() { return new Date().getTime(); }
