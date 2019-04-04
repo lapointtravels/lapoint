48e89c77a17538be40ad52805bff0f72c124bd58
rickard <reekrd@gmail.com>
2019-04-04 17:03:32 +0200

    Skip protocol in front of image url

--- wp-content/themes/lapoint2016/404.php
+++ wp-content/themes/lapoint2016/404.php
@@ -10,7 +10,7 @@ header("HTTP/1.0 404 Not Found");
 get_header(); ?>
 
 	<div class="full-image">
-		<img width="2500" height="618" src="http://www.lapointcamps.com/wp-content/uploads/2016/04/7-2500x618.jpg" class="attachment-header-image size-header-image wp-post-image" alt="080316_0860" srcset="http://www.lapointcamps.com/wp-content/uploads/2016/04/7-300x74.jpg 300w, http://www.lapointcamps.com/wp-content/uploads/2016/04/7-768x190.jpg 768w, http://www.lapointcamps.com/wp-content/uploads/2016/04/7-1024x253.jpg 1024w, http://www.lapointcamps.com/wp-content/uploads/2016/04/7-2500x618.jpg 2500w, http://www.lapointcamps.com/wp-content/uploads/2016/04/7-1200x297.jpg 1200w, http://www.lapointcamps.com/wp-content/uploads/2016/04/7-770x190.jpg 770w" sizes="(max-width: 2500px) 100vw, 2500px">
+		<img width="2500" height="618" src="//www.lapointcamps.com/wp-content/uploads/2016/04/7-2500x618.jpg" class="attachment-header-image size-header-image wp-post-image" alt="080316_0860" srcset="//www.lapointcamps.com/wp-content/uploads/2016/04/7-300x74.jpg 300w, //www.lapointcamps.com/wp-content/uploads/2016/04/7-768x190.jpg 768w, //www.lapointcamps.com/wp-content/uploads/2016/04/7-1024x253.jpg 1024w, //www.lapointcamps.com/wp-content/uploads/2016/04/7-2500x618.jpg 2500w, //www.lapointcamps.com/wp-content/uploads/2016/04/7-1200x297.jpg 1200w, www.lapointcamps.com/wp-content/uploads/2016/04/7-770x190.jpg 770w" sizes="(max-width: 2500px) 100vw, 2500px">
 	</div>
 
 	<div class="container mvl">
